1e0d9e2c1f984083341ddfe37361121b5bf8febb
tomasz <tomasztargo@gmail.com>
2017-06-22 09:20:46 +0200

    pierwsze deklaracja funkcji

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+//zdeklaracja zmiennych
+var pierwsza = 2
+var druga = 4
+
+//deklaracja funkcji z dwoma parametrami
+function getTriangleArea(a, h) {
+	if ((pierwsza <= 0) && (druga <= 0)){
+		console.log('Nieprawidłowe dane')
+	}
+}
+getTriangleArea (pierwsza, druga);
